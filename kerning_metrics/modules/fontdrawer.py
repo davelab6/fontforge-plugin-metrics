5fdf3f303bc132423f346536cd88c120d5fae611
lkcl <lkcl@teenymac.(none)>
2010-06-24 11:58:23 +0100

    add fill of letters

--- kerning_metrics/modules/fontdrawer.py
+++ kerning_metrics/modules/fontdrawer.py
@@ -43,44 +43,39 @@ class FontArea:
 
         print "is_quadratic", contour.is_quadratic
 
-        self.context.set_source_rgb(0, 0, 0)
+        if contour.isClockwise():
+            self.context.set_source_rgb(0, 0, 0)
+        else:
+            self.context.set_source_rgb(1, 1, 1)
         rect = self.da.get_allocation()
         x = rect.x #+ rect.width / 2
         y = rect.y #+ rect.height / 2
         offx += x
         offy += y
-        soffx, soffy = self.scale(rect, offx, offy)
         on_curve = False
-        for i in range(0): #range(len(contour)):
-            spx, spy = contour.getSplineAfterPoint(i)
-            print "spline", contour[i].x, contour[i].y, spx, spy
-            sx, sy = self.scale(rect, spx[0], spy[0])
-            self.context.move_to(sx, sy)
-            sx1, sy1 = self.scale(rect, spx[1], spy[1])
-            sx2, sy2 = self.scale(rect, spx[2], spy[2])
-            sx3, sy3 = self.scale(rect, spx[3], spy[3])
-            self.context.curve_to(sx1, sy1, sx2, sy2, sx3, sy3)
-            self.context.stroke()
         bez = []
         prev = None
+
+        p = contour[0]
+        sx, sy = self.scale(rect, p.x + offx, p.y + offy)
+        self.context.move_to(sx, sy)
+
         for p in contour + contour[0]:
             #print p.x, p.y, p.on_curve
             if not p.on_curve:
                 bez.append(p)
                 continue
-            if prev:
-                sx, sy = self.scale(rect, prev.x + offx, prev.y + offy)
-                self.context.move_to(sx, sy)
             if bez:
                 sx1, sy1 = self.scale(rect, bez[0].x+ offx, bez[0].y + offy)
                 sx2, sy2 = self.scale(rect, bez[1].x+ offx, bez[1].y + offy)
                 sx3, sy3 = self.scale(rect, p.x + offx, p.y + offy)
                 self.context.curve_to(sx1, sy1, sx2, sy2, sx3, sy3)
-                self.context.stroke()
                 bez = []
             elif prev:
                 sx, sy = self.scale(rect, p.x + offx, p.y + offy)
                 self.context.line_to(sx, sy)
-                self.context.stroke()
             prev = p
+
+        #self.context.stroke_preserve()
+        self.context.fill()
 
